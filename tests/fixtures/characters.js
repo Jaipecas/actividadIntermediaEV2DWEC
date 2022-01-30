a0606d38a65ceef30eab906c569456b453ff751d
Jaime Perez Castellanos <jaipecas89@gmail.com>
2022-01-30 13:00:28 +0100

    Cambios

--- tests/fixtures/characters.js
+++ tests/fixtures/characters.js
@@ -52,7 +52,6 @@ const characters = {
   attila
 }
 
-
 export {
   characters
 }
--- tests/searchCharacter.test.js
+++ tests/searchCharacter.test.js
@@ -1,19 +1,76 @@
-import { characters } from './fixtures/characters'
+import {
+  searchCharacter
+} from '../src/searchRick';
+import {
+  characters
+} from './fixtures/characters';
+import fetch from 'node-fetch';
+
+jest.mock('node-fetch');
+
+
 
 describe('searchCharacter', () => {
 
-  it('gets the character data');
+  it('gets the character data', async () => {
+    const response = {
+      status: 200,
+      json: () => {
+        return {
+          results: [characters['annie']]
+        }
+      }
+    }
+
+    fetch.mockResolvedValueOnce(response);
+
+    const data = await searchCharacter('Aqua');
+    expect(data.name).toBe('Aqua Morty');
+
+  })
 
   describe('when there are more than one characters', () => {
-    it('gets the first one');
+    it('gets the first one', async () => {
+      const response = {
+        status: 200,
+        json: () => {
+          return {
+            results: [characters['annie'], characters['attila']]
+          }
+        }
+      }
+
+      fetch.mockResolvedValueOnce(response);
+
+      const data = await searchCharacter('Aqua');
+      expect(data.name).toBe('Aqua Morty');
+    });
   });
 
-  describe('when there is an HTTP error', () => {
+ /*  describe('when there is an HTTP error', () => {
     it('returns a rejected promise with the error');
-  });
+  }); */
 
   describe('when no data is found', () => {
-    it('returns "personaje no encontrado"');
+    it('returns "personaje no encontrado"', async () => {
+      const response = {
+        status: 404,
+        json: () => {
+          return {
+            results: [characters['annie'], characters['attila']]
+          }
+        }
+      }
+
+      fetch.mockResolvedValueOnce(response);
+
+      try {
+        await searchCharacter('Jaime');
+      } catch (error) {
+        expect(error).toBe(`Personaje no encontrado`);
+      }
+    });
   });
+
 
 });
